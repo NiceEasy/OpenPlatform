ccfa03bba987a88079edda4a1647cece097d6bfb
Administrator <Administrator@MS-20170608JDNZ>
2017-12-21 15:50:28 +0800

    test second remark

--- src/java/com/qskh/openplatform/controller/Test.java
+++ src/java/com/qskh/openplatform/controller/Test.java
@@ -8,6 +8,12 @@ public class Test {
 		System.out.println("---------C------------");
 		System.out.println("---------D------------");
 		System.out.println("---------E------------");
+		
+		
+		
+		System.out.println("---------F------------");
+		System.out.println("---------G------------");
+		System.out.println("---------H------------");
 	}
 
 }
