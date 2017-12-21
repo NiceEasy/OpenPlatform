9f0c2226012bbedee70a4335b485c9d6c7577a91
Administrator <Administrator@MS-20170608JDNZ>
2017-12-21 15:47:55 +0800

    Test Frist remark

---
+++ src/java/com/qskh/openplatform/controller/Test.java
@@ -0,0 +1,13 @@
+package com.qskh.openplatform.controller;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("---------A------------");
+		System.out.println("---------B------------");
+		System.out.println("---------C------------");
+		System.out.println("---------D------------");
+		System.out.println("---------E------------");
+	}
+
+}
